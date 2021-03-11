a22850b5b0746cc23e7e52901669762fa59a44ec
Earcandy <Earcandy@EarCandys-MacBook-Pro.local>
2021-03-11 11:33:31 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded 를 분석해서 가져올수 있게하는 코드
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 을 분석해서 가져올수 있게하는 코드
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://Earcandy:956400@earcandy.3uomi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -10,7 +17,22 @@ mongoose.connect('mongodb+srv://Earcandy:956400@earcandy.3uomi.mongodb.net/myFir
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('안녕하세요!!!')
+})
+
+app.post('/register', (req, res) => {
+  
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  
+    const user = new User(req.body)
+
+   user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
